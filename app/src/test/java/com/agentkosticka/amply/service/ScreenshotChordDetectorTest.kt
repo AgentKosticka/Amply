9a7934f6c2e01a25f10737a460b7184eed1475d9
TheBosoYolo <xLalipop22@seznam.cz>
2026-08-01 14:36:02 +0200

    Optimizations

---
+++ app/src/test/java/com/agentkosticka/amply/service/ScreenshotChordDetectorTest.kt
@@ -0,0 +1,44 @@
+package com.agentkosticka.amply.service
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class ScreenshotChordDetectorTest {
+    @Test
+    fun powerBeforeVolumeMatchesWithinInclusiveWindow() {
+        val detector = ScreenshotChordDetector()
+
+        assertFalse(detector.onPowerDown(1_000L))
+        assertTrue(detector.onVolumeDown(1_250L))
+    }
+
+    @Test
+    fun volumeBeforePowerMatchesWithinInclusiveWindow() {
+        val detector = ScreenshotChordDetector()
+
+        assertFalse(detector.onVolumeDown(1_000L))
+        assertTrue(detector.onPowerDown(1_250L))
+    }
+
+    @Test
+    fun eventsOutsideWindowDoNotMatch() {
+        val powerFirst = ScreenshotChordDetector()
+        assertFalse(powerFirst.onPowerDown(1_000L))
+        assertFalse(powerFirst.onVolumeDown(1_251L))
+
+        val volumeFirst = ScreenshotChordDetector()
+        assertFalse(volumeFirst.onVolumeDown(2_000L))
+        assertFalse(volumeFirst.onPowerDown(2_251L))
+    }
+
+    @Test
+    fun resetClearsPreviousKeyTimes() {
+        val detector = ScreenshotChordDetector()
+        detector.onPowerDown(1_000L)
+
+        detector.reset()
+
+        assertFalse(detector.onVolumeDown(1_100L))
+    }
+}
