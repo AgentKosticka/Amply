a733501482ac2dc3fa028d44d4c0fc37f1c423d4
TheBosoYolo <xLalipop22@seznam.cz>
2026-08-04 22:04:44 +0200

    Fixed broken release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.agentkosticka.amply"
         minSdk = 29
         targetSdk = amplyTargetSdk
-        versionCode = 37
-        versionName = "1.2.19"
+        versionCode = 38
+        versionName = "1.2.20"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
